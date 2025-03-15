68154fd85e0449d25eb318c959dcf290b5f55c2b
proIBL <daowz.laguna.erwin@gmail.com>
2025-03-15 00:05:57 -0600

    DSS completo

--- static/script.js
+++ static/script.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const images = [
         "/static/images/Mapa%20Suelo.png",
         "/static/images/Mapa%20Suelo%20Apto.png",
+        "/static/images/Mapa%20Sismo%206_5.png",
+        "/static/images/Mapa%20Sismo%207_5.png",
+        "/static/images/Sismos%20de%20Peligro%20en%20Relacion%20a%20Posibles%20Suelos.png",
+        "/static/images/Pisos%20Mas%20Estables.png",
+        "/static/images/Mapa%20Chihuahua.png",
     ];
 
     let index = 0;
